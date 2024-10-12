73a693591daa7f79e78f17d94713618427e47c80
shahbaz-kamal <shahbazkamal384@gmail.com>
2024-10-13 02:34:35 +0600

    Project done

--- js/index.js
+++ js/index.js
@@ -162,5 +162,75 @@ const handleActive = (id, isActive) => {
   }
 };
 
+// *Load Latest Post
+
+const loadLatestPost = async () => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/retro-forum/latest-posts`
+  );
+  const data = await res.json();
+  // console.log(data[0].cover_image);
+  displayLatestPosts(data);
+};
+
+// *Displaying Latest Post
+
+const displayLatestPosts = (posts) => {
+  const latestPostContainer = document.getElementById("latest-post-container");
+
+  posts.forEach((post) => {
+    const { cover_image, profile_image, title, description, author } = post;
+
+    const div = document.createElement("div");
+    div.className =
+      "card bg-base-100 shadow-xl text-color-primary border border-color-card-tertiary bg-white";
+    div.innerHTML = `
+    <figure class="px-3 md:px-4 lg:px-6 pt-3 md:pt-4 lg:pt-6">
+              <img
+                src=${cover_image}
+                alt="Shoes"
+                class="rounded-xl"
+              />
+            </figure>
+            <div class="px-3 md:px-4 lg:px-6 mt-3 pb-3 md:pb-4">
+              <div class="flex items-center gap-2 opacity-60 mb-3">
+                <i class="fa-regular fa-calendar"></i>
+                <p>${
+                  author.posted_date ? author.posted_date : `Date Not available`
+                }</p>
+              </div>
+
+              <h3 class="font-extrabold text-base md:text-lg mb-3">
+              ${title ? title : `Title Not available`}
+              </h3>
+              <p class="opacity-60 text-sm md:text-base mb-3 md:mb-4">
+               ${description}
+              </p>
+              <div class="flex gap-3 md:gap-4 flex-shrink-0 items-center">
+                <!-- ?image div -->
+                <div class="w-11 h-11 rounded-full">
+                  <img
+                    class="w-full h-full object-cover rounded-full"
+                    src=${profile_image}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <h3 class="font-bold text-sm lg:text-base">
+                 ${author.name}
+                  </h3>
+                  <p class="opacity-60 text-xs lg:text-sm">${
+                    author.designation
+                      ? author.designation
+                      : `Designation not available`
+                  }</p>
+                </div>
+              </div>
+            </div>`;
+    latestPostContainer.appendChild(div);
+  });
+};
+
 // *calling function globaly
 loadAllPosts();
+loadLatestPost();
